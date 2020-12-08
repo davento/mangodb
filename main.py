925cdda84496378473f1216f529ca71771e2b862
jean.huby <50341023+jeanpaulHT@users.noreply.github.com>
2020-12-08 18:05:39 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -11,35 +11,39 @@ import string
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST , port=5432)
 
-client_cache = []
 
 def genText():
     return ''.join(random.choices(string.ascii_letters + string.digits, k=16))
 
 def poblate_by_clients(size):
+    client_cache = []
     for i in range(size):
         with conn:
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curr:
                 id = random.randint(10000000, 99999999)
                 while id in client_cache:
-                    id = random.randint(412135, 432658)
+                    id = random.randint(10000000, 99999999)
                 client_cache.append(id)
                 curr.execute("INSERT INTO Grot.Empresa (ruc, direccion, razonSocial) VALUES("+str(id)+ ", ' " + genText()+ " ',"+ str(id) +" )")
                 conn.commit()
+                print(1)
 
     #End session                
     curr.close()
     conn.close()
 
 def poblate_by_warehouse(size):
+    warehouse_cache = []
+
     for i in range(size):
         with conn:
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curr:
-                id = random.randint(10000000, 99999999)
-                while id in client_cache:
-                    id = random.randint(412135, 432658)
-                client_cache.append(id)
-                curr.execute("INSERT INTO Grot.almacen (direccion , descripcion , capacidad ) VALUES("+str(id)+ ", ' " + genText()+ " ',"+ str(id) +" )")
+                
+                dir = genText()
+                while dir in warehouse_cache:
+                    dir = genText()
+                warehouse_cache.append(dir)
+                curr.execute("INSERT INTO Grot.almacen (direccion , capacidad ) VALUES( ' "+str(dir)+ " ' , " + str(random.randint(1000, 10000)) +" )")
                 conn.commit()
 
     #End session                
@@ -47,5 +51,43 @@ def poblate_by_warehouse(size):
     conn.close()
 
 
+
+def poblate_by_products(size):
+    products_cache = []
+
+    for i in range(size):
+        with conn:
+            with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curr:
+                
+                id = random.randint(1, size * 10)
+                while id in products_cache:
+                    id =  random.randint(1, size * 10)
+                products_cache.append(id)
+                curr.execute("INSERT INTO Grot.producto (id  , nombre ) VALUES( "+ str(id)+ "  ,  ' " + genText() +" ' )")
+                conn.commit()
+
+    #End session                
+    curr.close()
+    conn.close()
+
+def get_keys(keys, table, rows = ""):
+    row = []
+    if(str(rows) != ""):
+        rows = "Limit " +  str(rows)
+
+    with conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curr:
+            curr.execute("SELECT " + str(keys)  +" FROM Grot." + str(table)  + " TABLESAMPLE BERNOULLI(1) " + rows)
+            row = curr.fetchall ()
+            conn.commit()
+    #End session                
+    curr.close()
+    conn.close()
+
+    return row
+
 if __name__ == "__main__":
-    poblate_by_clients(100)
+    row = get_keys("*" , "empresa")
+    for x in row:
+        print(x)
+            
