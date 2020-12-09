ae10130d5a3681a36a134c15a6b6acc6a9028fcf
Efraín Córdova <52300931+ecordova0975@users.noreply.github.com>
2020-12-09 01:20:15 -0500

    XD

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ def generate_comprobante(size):
                 conn.commit()
     #End session                
     curr.close()
-    conn.close()    
 
 def poblate_by_clients(size):
     client_cache = []
@@ -54,11 +53,9 @@ def poblate_by_clients(size):
                 client_cache.append(id)
                 curr.execute("INSERT INTO Grot.Empresa (ruc, direccion, razonSocial) VALUES("+str(id)+ ", ' " + genText()+ " ',"+ str(id) +" )")
                 conn.commit()
-                print(1)
 
     #End session                
     curr.close()
-    conn.close()
 
 def poblate_by_warehouse(size):
     warehouse_cache = []
@@ -76,35 +73,68 @@ def poblate_by_warehouse(size):
 
     #End session                
     curr.close()
-    conn.close()
 
 def gen_stock(size):
     keys_producto = get_keys(keys="id", table="producto", prob = 50, rows = size)
     keys_almacen = get_keys(keys="direccion", table="almacen", prob = 50, rows = size)
-    print(keys_producto)
-    for i in range(min(len(keys_producto), len(keys_almacen))):
+    sizeProducto = len(keys_producto) - 1
+    sizeAlmacen = len(keys_almacen) - 1
+
+    stock = {}
+    
+    for i in range(size):
         with conn:
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curr:
                 cantidad = random.randint(500, 10000)
-                curr.execute("INSERT INTO Grot.stock id , direccion , cantidad) VALUES( ' "+ str(keys_producto[i])+ "  ' , " + str(keys_almacen[i]) + "  ,  ' " + str(cantidad) + " )")
+                id_producto = str(keys_producto[random.randint(0, sizeProducto)][0])
+                id_almacen = str(keys_almacen[random.randint(0, sizeAlmacen)][0])
+                if id_almacen in stock:
+                    while id_producto in stock[id_almacen]:
+                        id_producto = str(keys_producto[random.randint(0, sizeProducto)][0])
+                else:
+                    stock[id_almacen] = []        
+                stock[id_almacen].append(id_producto)    
+                curr.execute("INSERT INTO Grot.stock (id , almacen , cantidad) VALUES( '"+ id_producto + "' , '" + id_almacen + "'  ,   " + str(cantidad) + " )")
                 conn.commit()
         #End session                
-        curr.close()
-        conn.close()
+    curr.close()
 
 def gen_compra(size):
-    keys_cliente = get_keys(keys = "ruc", table = "cliente", rows = 1, prob = 50)
-    keys_comprobante = get_keys(keys = "numero", table ="comprobante", rows = 1, prob = 50)
+    keys_cliente = get_keys(keys = "ruc", table = "empresa", rows = size, prob = 50)
+    keys_producto =  get_keys(keys="id", table="producto", prob = 50, rows = size)
+    keys_almacen = get_keys(keys="direccion", table="almacen", prob = 50, rows = size)
+    keys = get_keys(keys = "numero", table ="comprobante", prob = 100)
+    size_cliente = len(keys_cliente) - 1
+    size_comprobante = len(keys) - 1
+    size_producto = len(keys_producto) - 1
+    size_almacen = len(keys_almacen) - 1
 
     for i in range(size):
         with conn:
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curr:
-                curr.execute("INSERT INTO Grot.Compra () VALUES ()")
+                ruc = str(keys_cliente[random.randint(0, size_cliente)][0])
+                numero = str(keys[i][0])
+                curr.execute("INSERT INTO Grot.Compra (ruc, numero) VALUES ( "+ ruc + " , " + numero + " )" )
+                cantidad = random.randint(1, 45)
+                precioUnitario = random.randint(3, 750)
+                curr.execute("INSERT INTO Grot.CDetalle (id, direccion, numero, cantidad, precioUnitario) VALUES ( '" + str(keys_producto[random.randint(0,size_producto)][0]) + "' , '" + str(keys_almacen[random.randint(0,size_almacen)][0]) + "' , '" + numero + "' , '" + str(cantidad) + "' , '" + str(precioUnitario) + "' )")
                 conn.commit()
-                curr.execute("INSERT INTO Grot.Compra () VALUES ()")
     #End session                
     curr.close()
-    conn.close()
+    gen_venta(size, keys)
+
+def gen_venta(size, keys):
+    keys_cliente = get_keys(keys = "ruc", table = "empresa", rows = size, prob = 50)
+    size_ruc = len(keys_cliente) - 1
+    size_comprobante = len(keys) - 1
+
+    for i in range(size):
+        with conn:
+            with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curr:
+                curr.execute("INSERT INTO Grot.Venta (numero, ruc) VALUES ( '" + str(keys[i + size][0]) + "' , '" + str(keys_cliente[random.randint(0, size_ruc)][0]) + "' )")
+                conn.commit()
+    #End session                
+    curr.close()           
     
 def poblate_by_products(size):
     products_cache = []
@@ -121,7 +151,16 @@ def poblate_by_products(size):
                 conn.commit()
     #End session                
     curr.close()
-    conn.close()
+
+def fue():
+    with conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curr:
+            str = "SET search_path = '$user', Grot ; delete from cdetalle; delete from vdetalle; delete from compra; delete from venta; delete from comprobante; delete from stock; delete from cliente; delete from persona; delete from producto; delete from almacen; delete from empresa;"
+            curr.execute(str)
+            conn.commit()
+
+    curr.close()        
+
 
 def get_keys(keys, table, rows = "", prob = 1):
     row = []
@@ -133,9 +172,16 @@ def get_keys(keys, table, rows = "", prob = 1):
             curr.execute("SELECT " + str(keys)  +" FROM Grot." + str(table)  + " TABLESAMPLE BERNOULLI( " + str(prob) + ") " + rows)
             row = curr.fetchall ()
             conn.commit()
-
     return row
 
 if __name__ == "__main__":
-    gen_stock(100)
+    fue()
+    size = 10000
+    poblate_by_clients(size)
+    poblate_by_products(size)
+    poblate_by_warehouse(size)
+    gen_stock(size)
+    generate_comprobante(size * 2)
+    gen_compra(size)
+    conn.close()
             
